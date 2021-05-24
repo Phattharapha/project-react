2dbd610748c312256d46688da3898810f9dc5b8f
Phattharapha Tancharoen <phattharapha.tanc@gmail.com>
2021-05-25 06:19:41 +0700

    create login

--- projectreact/src/App.js
+++ projectreact/src/App.js
@@ -1,7 +1,7 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './App.css';
 import Login from './components/Login'
-
+import auth from './firebase'
 function App() {
   const [session, setSession] = useState({
     isLoggedIn: false,
@@ -9,13 +9,43 @@ function App() {
     errorMessage:null
   })
 
+
+  useEffect(() => {
+    const handleAuth = auth.onAuthStateChanged(user=>{
+      if (user) {
+        setSession({
+          isLoggedIn: true,
+          currentUser: user,
+          errorMessage:null
+        })
+      }
+    });
+    return () => {
+      handleAuth()
+    }
+  }, [])
+
+   const handleLogout=()=> {
+    auth.signOut().then(response => {
+      setSession({
+        isLoggedIn: false,
+        currentUser: null
+      })
+    })
+  }
+
   return (
     <div className="App">
       {
         session.isLoggedIn ? (<header className="App-header">
-        Somthing
+        Somthing , 
+        <h1> {session.currentUser && session.currentUser.email}</h1>
+        <button type="button" onClick={handleLogout}>
+          Logout
+        </button>
+        
       </header>) : (<header className="App-header">
-        <Login/>
+        <Login setSession={setSession}/>
       </header>)
       }
       
---
+++ projectreact/src/firebase/config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCT034Yqz7JVrSRTcv_gwreFPsPIB93YLI",
+  authDomain: "project-react-5bff8.firebaseapp.com",
+  projectId: "project-react-5bff8",
+  storageBucket: "project-react-5bff8.appspot.com",
+  messagingSenderId: "448941388542",
+  appId: "1:448941388542:web:999b85531e277d59b7abaf",
+  measurementId: "G-MHY8FCJXTX"
+};
+
+export default firebaseConfig;
